38840a3d08f4844b95dd4bac78326316b3016c06
siningsun <690058381@qq.com>
2025-07-19 15:49:25 +0800

    add datamodel

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type Recipe struct {
+	Name         string   `json:"name"`
+	Ingredients  []string `json:"ingredients"`
+	Instructions string   `json:"instructions"`
+	Tags         []string `json:"tags"`
+	PublishedAt  string   `json:"publishedAt"`
+}
+
 func main() {
 	router := gin.Default()
 	router.Run()
